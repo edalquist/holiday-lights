dba1e742fea5880b1a0dca6ff7aea89cc6a6c9fd
Eric Dalquist <eric.dalquist@gmail.com>
2017-10-31 07:45:18 -0700

    It's Alive!

--- src/holiday-lights.ino
+++ src/holiday-lights.ino
@@ -4,15 +4,213 @@
  * Author:
  * Date:
  */
+#include "ArduinoJson.h"
+#include <FastLED.h>
+FASTLED_USING_NAMESPACE
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+#define LED_PIN      D0
+#define COLOR_ORDER  GRB
+#define CHIPSET      WS2812B
+#define NUM_LEDS     16
+#define LEDS_PER_ROW 5
 
+#define BRIGHTNESS  250
+#define FRAMES_PER_SECOND 60
+
+CRGB leds[NUM_LEDS];
+
+// Gradient palette "GMT_hot_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/gmt/tn/GMT_hot.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 16 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( GMT_hot_gp ) {
+    0,   0,  0,  0,
+   95, 255,  0,  0,
+  191, 255,255,  0,
+  255, 255,255,255};
+
+// Gradient palette "bhw1_01_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_01.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw1_01_gp ) {
+    0, 227,101,  3,
+  117, 194, 18, 19,
+  255,  92,  8,192};
+
+// Gradient palette "bhw1_04_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_04.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw1_04_gp ) {
+    0, 229,227,  1,
+   15, 227,101,  3,
+  142,  40,  1, 80,
+  198,  17,  1, 79,
+  255,   0,  0, 45};
+
+DEFINE_GRADIENT_PALETTE( halloween_p ) {
+    0, 255, 90,  0,
+  170, 102,  0,102,
+  255, 102,  0,102};
+
+
+const auto ORANGE = CRGB(255, 90, 0);
+const auto PURPLE = CRGB(102, 0, 102);
+const auto GREEN = CRGB::ForestGreen;
+
+int brightness = BRIGHTNESS;
+int stripWidth = 2;
+
+int setBrightness(String bStr) {
+    const auto b = bStr.toInt();
+    if (b < 0) {
+        brightness = 0;
+    } else if (b > 255) {
+        brightness = 255;
+    } else {
+        brightness = b;
+    }
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+int setStripWidth(String wStr) {
+    const auto w = wStr.toInt();
+    if (w < 1) {
+        stripWidth = 1;
+    } else if (w > (NUM_LEDS / LEDS_PER_ROW)) {
+        stripWidth = (NUM_LEDS / LEDS_PER_ROW) - 1;
+    } else {
+        stripWidth = w;
+    }
+}
 
+void setup() {
+    delay(5000); // 10 second delay for recovery
+
+    Particle.variable("brightness", brightness);
+    Particle.variable("stripWidth", stripWidth);
+
+    Particle.function("setBright", setBrightness);
+    Particle.function("setWidth", setStripWidth);
+
+    delay(3000); // setup guard
+    FastLED.addLeds<CHIPSET, D0, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalPixelString );
+    FastLED.addLeds<CHIPSET, D1, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalPixelString );
+    FastLED.addLeds<CHIPSET, D2, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalPixelString );
+    FastLED.addLeds<CHIPSET, D3, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalPixelString );
+    FastLED.addLeds<CHIPSET, D4, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalPixelString );
+    FastLED.setBrightness(BRIGHTNESS);
+}
+
+
+void drawPumpkinPole() {
+    const int STEM_START = NUM_LEDS - (6 * LEDS_PER_ROW);
+    for (int n = 0; n < NUM_LEDS; n++) {
+        if (n < STEM_START) {
+            leds[n] = ORANGE;
+        } else {
+            leds[n] = GREEN;
+        }
+    }
+}
+
+const auto CHASE_DELAY = 25;
+
+void drawGhostPole() {
+    // This is the "clock" that determines how fast the animation runs
+    auto startIdx = ((millis() / CHASE_DELAY) % (NUM_LEDS / LEDS_PER_ROW)) * LEDS_PER_ROW;
+
+    for (auto n = 0; n < NUM_LEDS; n++) {
+        auto color = ORANGE;
+        if ((n / LEDS_PER_ROW / stripWidth) % 2 == 1) {
+            color = PURPLE;
+        }
+        leds[(startIdx + n) % NUM_LEDS] = color;
+    }
+}
+
+
+void drawRoofLine() {
+  auto startIdx = (millis() / CHASE_DELAY) % NUM_LEDS;
+
+  for (auto n = 0; n < NUM_LEDS; n++) {
+    auto wavePosition = ((float)(startIdx + n) / NUM_LEDS) * 255;
+    auto waveValue = cubicwave8(wavePosition);
+    leds[n] = ORANGE;
+    leds[n].nscale8_video(waveValue);
+  }
+}
+
+
+CRGBPalette16 currentPalette( CRGB::Blue );
+CRGBPalette16 targetPalette( CRGB::Blue );
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+  uint8_t flicker = 30;
+  uint8_t brightness = qadd8(sin8(colorIndex), 2 + flicker + random8(flicker)) - flicker;
+
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex + sin8(i*16), brightness);
+    colorIndex += 3;
+  }
+}
+
+void ChangePalettePeriodically()
+{
+  uint8_t secondHand = (millis() / 1000) % 60;
+  static uint8_t lastSecond = 99;
+  static uint8_t changeAccumulator = 0;
+
+  if( lastSecond != secondHand) {
+    lastSecond = secondHand;
+    changeAccumulator += random8(10);
+    if (changeAccumulator > 100) {
+      changeAccumulator = 0;
+      switch (random8(6)) {
+        case 0 : targetPalette = halloween_p; break;
+        case 1 : targetPalette = CRGBPalette16(ORANGE); break;
+        case 2 : targetPalette = CRGBPalette16(PURPLE); break;
+        case 3 : targetPalette = CRGBPalette16(CRGB::Red); break;
+        case 4 : targetPalette = CRGBPalette16(CRGB::Green); break;
+        case 5 : targetPalette = CRGBPalette16(CRGB::Blue); break;
+      }
+    }
+  }
+}
+
+void drawPumpkin() {
+    ChangePalettePeriodically();
+
+    // Crossfade current palette slowly toward the target palette
+    //
+    // Each time that nblendPaletteTowardPalette is called, small changes
+    // are made to currentPalette to bring it closer to matching targetPalette.
+    // You can control how many changes are made in each call:
+    //   - the default of 24 is a good balance
+    //   - meaningful values are 1-48.  1=veeeeeeeery slow, 48=quickest
+    //   - "0" means do not change the currentPalette at all; freeze
+
+    uint8_t maxChanges = 6;
+    nblendPaletteTowardPalette( currentPalette, targetPalette, maxChanges);
+
+
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
+    FillLEDsFromPaletteColors( startIndex);
+}
+
+
+
+void loop() {
+    drawPumpkin();
+    // drawGhostPole();
+    // drawRoofLine();
+
+    FastLED.setBrightness(brightness);
+    FastLED.show();
+    delay(1000 / FRAMES_PER_SECOND);
 }
